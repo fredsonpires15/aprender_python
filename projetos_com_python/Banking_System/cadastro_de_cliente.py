e9faefb24938dc07a71a054df9a4e8845a69ac52
fredsonpires15 <fredsonpires15@gmail.com>
2024-05-21 14:14:10 +0100

    Commit-08

--- projetos_com_python/Banking_System/cadastro_de_cliente.py
+++ projetos_com_python/Banking_System/cadastro_de_cliente.py
@@ -9,53 +9,130 @@ Cadastro de Clientes:
 # Criar uma classe para representar um cliente, com atributos como: nome, CPF, endereço, codigo_postal.
 class Cliente:
 
-    def __init__(self, nome, NIF, morada, codigo_postal):
+    def __init__(self, nome,idade,sexo,identificacao,n_identificacao,data_de_validade,estado_civil, NIF,pais,distrito,rua, codigo_postal, data_de_nasc,pais_de_nasc,telemovel,e_mail, saldo):
         self._nome = nome
+        self._idade = idade
+        self._sexo = sexo
+        self._identificacao = identificacao
+        self._n_identificacao = n_identificacao
+        self._data_de_validade = data_de_validade
+        self._estado_civil = estado_civil
         self._NIF:str = str(NIF)
-        self._morada = morada
+        self._pais = pais
+        self._distrito = distrito
+        self._rua = rua
         self._codigo_postal = codigo_postal
-        self._informacao_do_cliente = dict()
-    
+        self._data_de_nasc = data_de_nasc
+        self._pais_de_nasc = pais_de_nasc
+        self._telemovel = telemovel
+        self._email = e_mail
+        self._saldo = saldo
+        self._informacao_do_cliente = dict() 
+        
     def nome(self):  # retorna um nome
         return self._nome
     
+    def idade(self):
+        return self._idade
+    
+    def sexo(self):
+        return self._sexo
+    
+    def identificacao(self):
+        return self._identificacao
+    
+    def n_identificacao(self):
+        return self._n_identificacao
+    
+    def data_de_validade(self):
+        return self._data_de_validade
+    
+    def estado_civil(self):
+        return self._estado_civil
+
     def nif(self): # retorna um NIF
         return self._NIF
+     
+    # ---Dados da Residência------------------------
+    def pais(self):
+        return self._pais
     
-    def morada(self): # retorna um Morada
-        return self._morada
+    def distrito(self):
+        return self._distrito
     
+    def rua(self):
+        return self._rua
+        
     def codigo_postal(self): # retorna um código-postal
         return self._codigo_postal
     
+    # ---Naturalidade----------------------
+
+    def data_de_nasc(self): 
+        return self._data_de_nasc
+    
+    def pais_de_nasc(self): 
+        return self._pais_de_nasc
+    
+    #---Contacto--------------------------
+
+    def telemovel(self):
+        return self._telemovel
+    
+    def email(self):
+        return self._email
+    
+    # -----Dados da Conta----------
+
+    def saldo(self):
+        return self._saldo 
+
 
     # Guardar as informções do cliente num dicionário
     def coletar_dados_do_cliente(self):
-        self._informacao_do_cliente['Nome'] = self.nome()
-        self._informacao_do_cliente['NIF'] = self.nif()
-        self._informacao_do_cliente['Morada'] = self.morada()
-        self._informacao_do_cliente['Codigo-Postal'] = self.codigo_postal()
+        # self._informacao_do_cliente['Nome'] = self.nome()
+        # self._informacao_do_cliente['NIF'] = self.nif()
+        # self._informacao_do_cliente['Morada'] = self.morada()
+        # self._informacao_do_cliente['Codigo-Postal'] = self.codigo_postal() 
+        return dict(
+            Nome=self.nome(),
+            Idade=self.idade(),
+            Sexo=self.sexo(),
+            Identificação=self.identificacao(),
+            N_Identificação=self.n_identificacao(),
+            Data_de_Validade=self.data_de_validade(),
+            Estado_Civil=self.estado_civil(),
+            NIF=self.nif(),
+            País=self.pais(),
+            Distrito=self.distrito(),
+            Rua=self.rua(),
+            Codigo_Postal=self.codigo_postal(),
+            Data_de_Nasc=self.data_de_nasc(),
+            País_de_Nasc=self.pais_de_nasc(),
+            Telemóvel=self.telemovel(),
+            Email=self.email(),
+            Saldo=self.saldo()
 
-        return self._informacao_do_cliente  # Retornar as informações num dicionário
+        )  # Retornar as informações num dicionário
     
     
 # Implementar classe e funções para permitir que novos clientes se cadastrem no sistema, fornecendo suas informações pessoais   
 class CadastrarCliente(Cliente):
 
-    def __init__(self, nome, NIF, morada, codigo_postal):
-        super().__init__(nome, NIF, morada, codigo_postal)
+    def __init__(self,nome,idade,sexo,identificacao,n_identificacao,data_de_validade,estado_civil, NIF,pais,distrito,rua, codigo_postal, data_de_nasc,pais_de_nasc,telemovel,e_mail, saldo):
+        super().__init__(nome,idade,sexo,identificacao,n_identificacao,data_de_validade,estado_civil, NIF,pais,distrito,rua, codigo_postal, data_de_nasc,pais_de_nasc,telemovel,e_mail,saldo)
         #self._cliente = {}
         self.arquivo = "C:\\Users\\freds\\Desktop\\curso_de_programacao\\aprender_python\\Projetos_com_python\\Banking_System\\Banco_de_Dados.xlsx"
 
     def banco_de_dados(self):
-        # carregar uma arquivo Excel existente ou criar um DataFrame vazio
+        # carregar uma arquivo Excel existente ou criar um DataFrame vazio 
         try:
-            df = pd.read_excel(self.arquivo) # Tenta carregar uma arquivo Excel existente
+            df = pd.read_excel(self.arquivo)  # Tenta carregar uma arquivo Excel existente
         except FileNotFoundError:
-            df = pd.DataFrame(columns=['Nome', 'NIF', 'Morada', 'Codigo-Postal'])
+            df = pd.DataFrame(columns=['Nome', 'Idade', 'Sexo', 'Identificação','N_Identificação','Data_de_Validade', 'Estado_Civil', 'NIF', 'País', 'Distrito', 'Rua','Codigo_Postal', 'Data_de_Nasc', 'País_de_Nasc', 'Telemóvel', 'Email', 'Saldo' ])
 
 
-        # coletar os dados do cliente
+        # coletar os dados do cliente 
         cliente = self.coletar_dados_do_cliente()
 
         #criar um DataFrame com os dados do cliente
--- projetos_com_python/Banking_System/teste.py
+++ projetos_com_python/Banking_System/teste.py
@@ -2,6 +2,7 @@ from cadastro_de_cliente import Cliente, CadastrarCliente
 from colorama import Fore, Back, Style
 from validar import validar_usuario
 from estrutura import menu_login, menu
+import os
 
 
 print()
@@ -15,18 +16,52 @@ if __name__ == '__main__':
     while True:
         menu_login() # fazer login ou cadastro
         op = int(input('Opção: ') )
+        os.system('cls')
 
         if op == 1:
+                print('------------Sua Identificação--------------')
                 nome = input('Nome: ')
+                idade = input('Idade: ')
+                sexo = input('Sexo [M/F]: ').upper()
+                identificacao = input('Identificação [PC/T.R]: ')
+                n_identificacao = input('Nº de Identificação: ')
+                data_de_validade = input('Data de validade: ')
+                estado_civil = input('Estado Civil: ')
+                NIF = input('NIF: ')
+                print('------------------------------------- ') 
+
+
+                print('-------------Residência---------------') 
+                pais = input('País: ')
+                distrito = input('Distrito: ')
+                rua = input('Rua: ')
+                codigo_postal = input('Código-Postal: ')
+                print('------------------------------------- ') 
+
+
+                print('------------Naturalidade------------- ') 
+                data_de_nasc = input('Data de Nascimento:    ')
+                pais_de_nasc = input('País: ')
+                print('------------------------------------- ') 
+
+
+                print('--------------Contacto--------------- ') 
+                telemovel = input('Telemóvel: ')
+                e_mail = input('E-mail: ')
+                print('------------------------------------- ') 
                 
-                NIF = input('NIF: ') 
-                morada = input('Morada: ') 
-                codigo_postal = input('Código Postal: ') 
+                os.system('cls')
+                print('------------Abrir Conta-------------- ')
+                
+                print('Para concluir o processo, faça um depósito de no minimo 100€. \n Nota: Depois da abertura da conta o cliente pode poderá sacar o seu dinheiro quando quiser!!  ')
+                saldo = float(input('Montante(€): '))
+                
+
 
                 # Criando um novo cliente
-                novo_cliente = Cliente(nome, NIF, morada, codigo_postal)
+                novo_cliente = Cliente(nome,idade,sexo,identificacao,n_identificacao,data_de_validade,estado_civil, NIF,pais,distrito,rua, codigo_postal, data_de_nasc,pais_de_nasc,telemovel,e_mail, saldo)
 
-                cadastro = CadastrarCliente(nome, NIF, morada, codigo_postal)
+                cadastro = CadastrarCliente(nome,idade,sexo,identificacao,n_identificacao,data_de_validade,estado_civil, NIF,pais,distrito,rua, codigo_postal, data_de_nasc,pais_de_nasc,telemovel,e_mail, saldo)
 
                 cadastro.banco_de_dados()
 
@@ -39,21 +74,65 @@ if __name__ == '__main__':
         elif op == 2:
                 nome = input('Nome do usúario: ')
                 senha = input('Senha: ')
+                os.system('cls')
+
                 if  validar_usuario(nome,senha, nome_valido, senha_valida):
                         while True:
+                                
                                 menu()
                                 op = int(input('Opção: ') )
+                                os.system('cls')
                                 if op == 1:
-                                    print(' Mostrar Dados do usuário...')
+                                        print(' Mostrar Dados do usuário...')
+                                        print('---identificação---------------------')
+                                        print('|                                   |')
+                                        print('|                 Nome:             |')
+                                        print('|                 Sexo:             |')
+                                        print('|Identificação [PC/TR]:             |')
+                                        print('|      Nºidentificação:             |')
+                                        print('|     Data de validade:             |')
+                                        print('|                  NIF:             |')
+                                        print('|         Estado Civil:             |')
+                                        print('-------------------------------------')
+                                        print('')
+                                        print('---Residência------------------------')
+                                        print('|                                   |')
+                                        print('|                 País:             |')
+                                        print('|             Distrito:             |')
+                                        print('|                  Rua:             |')
+                                        print('|        Código-Postal:             |')
+                                        print('-------------------------------------')
+                                        print('')
+                                        print('---Naturalidade----------------------')
+                                        print('|                                   |')
+                                        print('|     Data de Nascimento:           |')
+                                        print('|                   País:           |')
+                                        print('|               Distrito:           |')
+                                        print('|                    Rua:           |')
+                                        print('-------------------------------------')
+                                        print('')
+                                        print('---Contacto--------------------------')
+                                        print('|                                   |')
+                                        print('|              Telemóvel:           |')
+                                        print('|                 E-mail:           |')
+                                        print('-------------------------------------')
+                                        depositar = input('Montante(€): ')
+                                        os.system('cls')
                                 elif op == 2:
                                     print(' Realizar um deposito...')
+                                    depositar = input('Montante(€): ')
+                                    os.system('cls')    
                                 elif op == 3:
                                     print(' Sacar dinheiro na conta...')
+                                    sacar = input('Montante(€): ') 
+                                    os.system('cls')
                                 elif op == 4:
                                     print(' Fazer uma transferência...')
                                 elif op == 5:
                                     print(' Saindo da conta...')
                                     break
+
+                                
                 else:
                        print('Nome ou senha. \033[1;31m Incorreta!!\033[0;0m ')
         
